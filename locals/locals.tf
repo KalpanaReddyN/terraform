7157aa7908a6d1192869aee5626dabc570185c44
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-19 22:04:16 +0100

    terraform

---
+++ locals/locals.tf
@@ -0,0 +1,4 @@
+locals {
+    domain_name = "kalpanadevops.online"
+    zone_id = "Z01584833LT7LVUACGA1"
+}
---
+++ locals/variables.tf
@@ -0,0 +1,14 @@
+variable "instance_name" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+    description = "description"
+}
+
+# variable "zone_id" {
+#     default = "Z01584833LT7LVUACGA1"
+# }
+
+# variable "domain_name" {
+#     default = "kalpanadevops.online"
+# }
+
