c1f6a8ec949854bae1f610278f4dde698fc2f65e
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-09-22 12:03:29 +0100

    terraform

---
+++ dynamic/variables.tf
@@ -0,0 +1,33 @@
+variable "ingress_rules" {
+    default = [
+        {
+            from_port = 22
+            to_port = 22
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 22"
+        },
+        {
+            from_port = 8080
+            to_port = 8080
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 8080"
+        },
+        {
+            from_port = 80
+            to_port = 80
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 80"
+        },
+        {
+            from_port = 3306
+            to_port = 3306
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 3306"
+        }
+        
+    ]
+}
