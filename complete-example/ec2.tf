1d76b503922332637daa8320e701057cfe3f3ffd
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-12-09 11:36:19 +0000

    terraform

--- complete-example/ec2.tf
+++ complete-example/ec2.tf
@@ -23,12 +23,14 @@ resource "aws_security_group" "ssh_sg_terraform" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"  
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
+    dynamic "ingress" {
+        for_each = var.ingress_rules
+        content {
+            from_port        = ingress.value["from_port"]
+            to_port          = ingress.value["to_port"]
+            protocol         = ingress.value["protocol"]  
+            cidr_blocks      = ingress.value["cidr_blocks"]
+        }
     }
 
     tags = {
